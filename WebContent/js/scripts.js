079a59bd89e8c8f237ae0ced0acad28c6a9a6ec8
jpmanne <jayaprakash.manne@sciits.com>
2016-10-14 09:11:52 +0530

    Updated

--- WebContent/js/scripts.js
+++ WebContent/js/scripts.js
@@ -1,5 +1,35 @@
 var myApp = angular.module('myModule', []);
 
 myApp.controller('myController', function ($scope) {
-    $scope.message = 'AngularJS Tutorial';
+    $scope.message = 'AngularJS Tutorial....';
+    var employee = {firstName  : 'Jaya Prakash', lastName  : 'Manne', gender  : 'Male'};
+    var employees = [
+                     	{firstName  : 'Jaya Prakash', lastName  : 'Manne', gender  : 'Male'},
+                     	{firstName  : 'Radha Devi', lastName  : 'Manne', gender  : 'Female'},
+                     	{firstName  : 'Cherish Keshto', lastName  : 'Manne', gender  : 'Male'}, 
+                     	{firstName  : 'Udvitha Keshto', lastName  : 'Manne', gender  : 'Female'} 
+                    ];
+    var countries = [
+                     	{name : "India", cities : [
+                     	                           	{name: "Hyderabad"},
+                     	                           	{name: "Delhi"},
+                     	                           	{name: "Vizag"}
+                     	                          ]
+                     	},
+                     	{name : "USA", cities : [
+                    	                           	{name: "Los Angeles"},
+                    	                           	{name: "Chicago"},
+                    	                           	{name: "Texas"}
+                    	                          ]
+                    	},
+                    	{name : "UK", cities : [
+                 	                           	{name: "London"},
+                 	                           	{name: "Barmingham"},
+                 	                           	{name: "Manchestor"}
+                 	                          ]
+                    	}
+                    ];
+    $scope.employee = employee;
+    $scope.employees = employees;
+    $scope.countries = countries;
 });
